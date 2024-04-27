b557c7a727a12cb54f26a113c22a0185bc8d8cf9
keshavsharma25 <skeshav0825@gmail.com>
2024-04-28 04:36:03 +0530

    add autocmd and keymaps

--- lua/keshav/init.lua
+++ lua/keshav/init.lua
@@ -1,4 +1,26 @@
 require 'keshav.options'
 require 'keshav.lazyload'
 
-require('keshav.keymaps').init()
+local K = require('keshav.keymaps')
+
+K.init()
+K.preventMs()
+
+
+local keshavGroup = vim.api.nvim_create_augroup('Keshav', { clear = true }),
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = keshavGroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  desc = 'Clear whitespaces at the end of line',
+  group = keshavGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
