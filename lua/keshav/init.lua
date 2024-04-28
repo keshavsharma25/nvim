56f17fd2439c5d5f47b49aa13f3b7b5fe16d9d81
keshavsharma25 <skeshav0825@gmail.com>
2024-04-28 20:52:13 +0530

    format & add lspAttach autocmd

--- lua/keshav/init.lua
+++ lua/keshav/init.lua
@@ -1,26 +1,44 @@
-require 'keshav.options'
-require 'keshav.lazyload'
+require('keshav.options')
+require('keshav.lazyload')
 
 local K = require('keshav.keymaps')
 
 K.init()
 K.preventMs()
 
-
-local keshavGroup = vim.api.nvim_create_augroup('Keshav', { clear = true }),
+local keshavGroup = vim.api.nvim_create_augroup('Keshav', { clear = true })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = keshavGroup,
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = keshavGroup,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  desc = 'Clear whitespaces at the end of line',
-  group = keshavGroup,
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
+    desc = 'Clear whitespaces at the end of line',
+    group = keshavGroup,
+    pattern = '*',
+    command = [[%s/\s\+$//e]],
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP Attach',
+    group = keshavGroup,
+    callback = function(e)
+        K.lsp(e)
+        local client = vim.lsp.get_client_by_id(e.data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                buffer = e.buf,
+                callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                buffer = e.buf,
+                callback = vim.lsp.buf.clear_references,
+            })
+        end
+    end,
+})
