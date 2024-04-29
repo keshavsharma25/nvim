886a1135fa787b7f7425b777436567dc434bc04a
keshavsharma25 <skeshav0825@gmail.com>
2024-04-29 15:28:15 +0530

    add event, keys and solidity-formatter

--- lua/keshav/plugins/conform.lua
+++ lua/keshav/plugins/conform.lua
@@ -1,6 +1,17 @@
 return {
     -- Autoformat
     'stevearc/conform.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    keys = {
+        {
+            '<F3>',
+            function()
+                require('conform').format({ async = false, lsp_fallback = true })
+            end,
+            mode = { 'n', 'v' },
+            desc = 'Conform: Format buffer',
+        },
+    },
     opts = {
         notify_on_error = false,
         format_on_save = {
@@ -9,15 +20,27 @@ return {
         },
         formatters_by_ft = {
             lua = { 'stylua' },
-            -- Conform can also run multiple formatters sequentially
-            -- python = { "isort", "black" },
-            --
-            -- You can use a sub-list to tell conform to run *until* a formatter
-            -- is found
-            javascript = { { 'prettier', 'biome' } },
-            typescript = { { 'prettier', 'biome' } },
-            typescriptreact = { { 'prettier', 'biome' } },
-            solidity = { { 'prettier' } },
+            go = { 'goimports', 'gofmt' },
+            rust = { 'rustfmt' },
+            python = { 'ruff_fix', 'ruff_format' },
+            javascript = { { 'prettierd', 'biome' } },
+            typescript = { { 'prettierd', 'biome' } },
+            typescriptreact = { { 'prettierd', 'biome' } },
+            solidity = { 'prettier_solidity' },
+            json = { 'prettier' },
+            ['*'] = { 'trim_whitespace' },
         },
+        formattters = {},
     },
+    config = function(_, opts)
+        require('conform').setup(opts)
+
+        local solidity_formatter =
+            vim.deepcopy(require('conform.formatters.prettier'))
+        require('conform.util').add_formatter_args(solidity_formatter, {
+            '--stdin-filepath',
+            '--plugin=prettier-plugin-solidity',
+        }, { append = false })
+        require('conform').formatters.prettier_solidity = solidity_formatter
+    end,
 }
