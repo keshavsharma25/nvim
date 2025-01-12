b90d1475c70fb6067c47da94aec01e5687da4aea
keshavsharma25 <skeshav0825@gmail.com>
2025-01-12 11:38:27 +0530

    add multi select in telescope

--- lua/keshav/plugins/telescope.lua
+++ lua/keshav/plugins/telescope.lua
@@ -15,7 +15,33 @@ return {
         { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
-        require('telescope').setup({
+        local telescope = require('telescope')
+        local actions = require('telescope.actions')
+        local action_state = require('telescope.actions.state')
+
+        local custom_actions = {}
+
+        function custom_actions.fzf_multi_select(prompt_bufnr)
+            local function get_table_size(t)
+                local count = 0
+                for _ in pairs(t) do
+                    count = count + 1
+                end
+                return count
+            end
+
+            local picker = action_state.get_current_picker(prompt_bufnr)
+            local num_selections = get_table_size(picker:get_multi_selection())
+
+            if num_selections > 1 then
+                actions.send_selected_to_qflist(prompt_bufnr)
+                actions.open_qflist()
+            else
+                actions.file_edit(prompt_bufnr)
+            end
+        end
+
+        telescope.setup({
             extensions = {
                 ['ui-select'] = { require('telescope.themes').get_dropdown() },
             },
@@ -23,6 +49,7 @@ return {
                 mappings = {
                     n = {
                         ['q'] = require('telescope.actions').close,
+                        ['<CR>'] = custom_actions.fzf_multi_select,
                     },
                 },
             },
