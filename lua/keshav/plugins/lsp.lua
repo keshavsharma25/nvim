6847a6685da00d44ec3178dd30734cb10d3aa832
keshavsharma25 <skeshav0825@gmail.com>
2025-01-15 04:24:59 +0530

    recaliberated lsp
    Compatible with python, typescript, and lua

--- lua/keshav/plugins/lsp.lua
+++ lua/keshav/plugins/lsp.lua
@@ -1,114 +1,184 @@
 return {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/nvim-cmp',
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'j-hui/fidget.nvim',
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {
+                'folke/lazydev.nvim',
+                ft = 'lua', -- only load on lua files
+                opts = {
+                    library = {
+                        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+                    },
+                },
+            },
+            { -- optional cmp completion source for require statements and module annotations
+                'hrsh7th/nvim-cmp',
+                opts = function(_, opts)
+                    opts.sources = opts.sources or {}
+                    table.insert(opts.sources, {
+                        name = 'lazydev',
+                        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+                    })
+                end,
+            },
+            'hrsh7th/cmp-nvim-lsp',
+            'saadparwaiz1/cmp_luasnip',
+            'L3MON4D3/LuaSnip',
+            'j-hui/fidget.nvim',
+            'stevearc/conform.nvim',
+            { 'https://git.sr.ht/~whynothugo/lsp_lines.nvim' },
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+        },
+        config = function()
+            local lspconfig = require('lspconfig')
+            local lsp_keymaps = require('keshav.keymaps')
+
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lsp_group =
+                vim.api.nvim_create_augroup('LspConfig', { clear = true })
+
+            local servers = { 'lua_ls', 'ruff', 'pyright', 'biome', 'ts_ls' }
+            local fmters = { 'prettierd' }
+
+            require('mason').setup({
+                ensure_installed = fmters,
+                automatic_installation = false,
+            })
+
+            require('mason-lspconfig').setup({
+                ensure_installed = servers,
+                automatic_installation = false,
+            })
+
+            for _, lsp in ipairs(servers) do
+                lspconfig[lsp].setup({
+                    -- on_attach = my_custom_on_attach,
+                    capabilities = capabilities,
+                })
+            end
+
+            local function setup_python(client, args)
+                lsp_keymaps.lsp(args)
+
+                -- Configure Python specific settings
+                if client.name == 'pyright' then
+                    -- Configure pyright specific settings
+                    client.server_capabilities.documentFormattingProvider =
+                        false -- Let ruff_lsp handle formatting
+                end
+
+                if client.name == 'ruff' then
+                    -- Configure Ruff specific settings
+                    client.server_capabilities.hoverProvider = false -- Let pyright handle hover
+                end
+            end
+
+            -- Check if the formatter is Biome or Prettier
+            local function get_formatter()
+                -- Check for Prettier config files
+                local prettier_files = {
+                    '.prettierrc',
+                    '.prettierrc.json',
+                    '.prettierrc.yml',
+                    '.prettierrc.yaml',
+                    '.prettierrc.json5',
+                    '.prettierrc.js',
+                    'prettier.config.js',
+                    'package.json',
+                }
+
+                return vim.lsp.util.root_pattern(unpack(prettier_files))(
+                    vim.fn.getcwd()
+                ) and 'prettierd' or 'biome'
+            end
+
+            local function setup_ts(client, args)
+                lsp_keymaps.lsp(args)
+
+                if client.name == 'ts_ls' then
+                    client.server_capabilities.documentFormattingProvider =
+                        false -- Let biome handle formatting
+
+                    client.root_dir = vim.lsp.util.root_pattern(
+                        'tsconfig.json',
+                        'jsconfig.json',
+                        'package.json',
+                        '.git'
+                    )
+
+                    client.single_file = false
+                end
+
+                if client.name == 'biome' then
+                    client.root_dir =
+                        vim.lsp.util.root_pattern('biome.json', 'biome.jsonc')
+
+                    client.single_file_support = true
+
+                    if get_formatter() == 'prettierd' then
+                        client.server_capabilities.documentFormattingProvider =
+                            false -- Let prettier handle formatting
+                    end
+                end
+            end
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local cl = vim.lsp.get_client_by_id(args.data.client_id)
+
+                    if not cl or not cl.server_capabilities then
+                        return
+                    end
+
+                    -- Enable inlay hints if supported
+                    if cl.server_capabilities.inlayHintProvider then
+                        vim.lsp.inlay_hint.enable(true)
+                    end
+
+                    -- Python (pyright, ruff) on LspAttach config
+                    if cl.name == 'pyright' or cl.name == 'ruff' then
+                        setup_python(cl, args)
+
+                        -- Setup Python specific autocommands
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            group = lsp_group,
+                            buffer = args.buf,
+                            callback = function()
+                                -- Format on save using the LSP
+                                vim.lsp.buf.format({
+                                    filter = function()
+                                        -- Prefer ruff_lsp for formatting
+                                        return cl.name == 'ruff'
+                                    end,
+                                    bufnr = args.buf,
+                                })
+                            end,
+                        })
+                    end
+
+                    if cl.name == 'ts_ls' then
+                        setup_ts(cl, args)
+
+                        -- Setup Typescript specific autocommands
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            group = lsp_group,
+                            buffer = args.buf,
+                            callback = function()
+                                -- Format on save using the LSP
+                                vim.lsp.buf.format({
+                                    filter = function()
+                                        -- Prefer biome for formatting
+                                        return cl.name == get_formatter()
+                                    end,
+                                    bufnr = args.buf,
+                                })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
     },
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require('cmp_nvim_lsp')
-        local capabilities = vim.tbl_deep_extend(
-            'force',
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
-
-        require('fidget').setup()
-        require('mason').setup()
-        require('mason-lspconfig').setup({
-            ensure_installed = {
-                'lua_ls',
-                'rust_analyzer',
-                'ts_ls',
-                'bashls',
-                'clangd',
-                'gopls',
-                'markdown_oxide',
-                'ruff',
-                'sqlls',
-                'solidity_ls',
-                'tailwindcss',
-            },
-            handlers = {
-                function(server_name)
-                    require('lspconfig')[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                ['lua_ls'] = function()
-                    local lspconfig = require('lspconfig')
-                    lspconfig.lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = 'Lua 5.4' },
-                                diagnostics = {
-                                    globals = {
-                                        'vim',
-                                        'it',
-                                        'describe',
-                                        'before_each',
-                                        'after_each',
-                                    },
-                                },
-                            },
-                        },
-                    })
-                end,
-
-                ['solidity_ls'] = function()
-                    require('lspconfig').solidity_ls.setup({
-                        capabilities = capabilities,
-                    })
-                end,
-            },
-        })
-
-        require('luasnip.loaders.from_vscode').lazy_load()
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-Space>'] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            }),
-        })
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = 'minimal',
-                border = 'rounded',
-                source = 'always',
-                header = '',
-                prefix = '',
-            },
-        })
-    end,
 }
