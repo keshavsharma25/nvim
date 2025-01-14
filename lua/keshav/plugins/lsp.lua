f50efbf0adb7643d59974377c7e5aa0d444e66a0
keshavsharma25 <skeshav0825@gmail.com>
2025-01-15 05:05:41 +0530

    add luasnip cmp

--- lua/keshav/plugins/lsp.lua
+++ lua/keshav/plugins/lsp.lua
@@ -179,6 +179,39 @@ return {
                     end
                 end,
             })
+
+            local luasnip = require('luasnip')
+            local cmp = require('cmp')
+
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = {
+                    ['<C-n>'] = cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Insert,
+                    }),
+                    ['<C-p>'] = cmp.mapping.select_prev_item({
+                        behavior = cmp.SelectBehavior.Insert,
+                    }),
+                    ['<CR>'] = cmp.mapping(
+                        cmp.mapping.confirm({
+                            behavior = cmp.ConfirmBehavior.Insert,
+                            select = true,
+                        }),
+                        { 'i', 'c' }
+                    ),
+                    ['<C-Space>'] = cmp.mapping.complete({}),
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                },
+            })
         end,
     },
 }
