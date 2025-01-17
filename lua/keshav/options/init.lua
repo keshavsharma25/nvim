fecef7398c2740e1354c57d762137f8227b03397
keshavsharma25 <skeshav0825@gmail.com>
2025-01-17 13:25:34 +0530

    fix: change ascii code for indent char

--- lua/keshav/options/init.lua
+++ lua/keshav/options/init.lua
@@ -25,7 +25,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '| ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '⏐ ', trail = '·', nbsp = '␣' }
 
 vim.opt.inccommand = 'split'
 
@@ -37,3 +37,5 @@ vim.opt.hlsearch = true
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+vim.g.swapfile = false
