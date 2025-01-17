1e89747e8558a657c5912e548831f102e25c4011
keshavsharma25 <skeshav0825@gmail.com>
2025-01-17 15:20:19 +0530

    update treesitter config

--- lua/keshav/plugins/treesitter.lua
+++ lua/keshav/plugins/treesitter.lua
@@ -6,23 +6,26 @@ return {
         require('nvim-treesitter.configs').setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                'vimdoc',
-                'javascript',
-                'typescript',
-                'c',
-                'lua',
-                'rust',
-                'jsdoc',
                 'bash',
+                'c',
+                'go',
+                'javascript',
+                'jsdoc',
+                'lua',
+                'python',
+                'rust',
                 'solidity',
+                'sway',
+                'typescript',
+                'vimdoc',
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+            sync_install = true,
 
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-            auto_install = true,
+            auto_install = false,
 
             indent = {
                 enable = true,
@@ -38,18 +41,8 @@ return {
                 -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = { 'markdown' },
             },
+            ignore_install = {},
+            modules = {},
         })
-
-        local treesitter_parser_config =
-            require('nvim-treesitter.parsers').get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = 'https://github.com/vrischmann/tree-sitter-templ.git',
-                files = { 'src/parser.c', 'src/scanner.c' },
-                branch = 'master',
-            },
-        }
-
-        vim.treesitter.language.register('templ', 'templ')
     end,
 }
