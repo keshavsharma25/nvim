4a479bd46673359668eaeab0e77eedcdd2b9e52e
keshavsharma25 <skeshav0825@gmail.com>
2025-03-13 16:56:15 +0530

    add wsl -> clipboard compatibility

--- lua/keshav/options/init.lua
+++ lua/keshav/options/init.lua
@@ -39,3 +39,18 @@ vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 
 vim.g.swapfile = false
+
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'win32yank-wsl',
+        copy = {
+            ['+'] = 'win32yank.exe -i --crlf',
+            ['*'] = 'win32yank.exe -i --crlf',
+        },
+        paste = {
+            ['+'] = 'win32yank.exe -o --lf',
+            ['*'] = 'win32yank.exe -o --lf',
+        },
+        cache_enabled = 0,
+    }
+end
