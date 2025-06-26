5b15b82a2b2cb8146f093b0cd1d2806ff553db0d
keshavsharma25 <skeshav0825@gmail.com>
2025-06-26 14:33:04 +0530

    add mkdp_port

--- lua/keshav/plugins/markdown_preview.lua
+++ lua/keshav/plugins/markdown_preview.lua
@@ -5,4 +5,8 @@ return {
     build = function()
         vim.fn['mkdp#util#install']()
     end,
+    init = function()
+        vim.g.mkdp_filetypes = { 'markdown' }
+        vim.g.mkdp_port = '2999'
+    end,
 }
