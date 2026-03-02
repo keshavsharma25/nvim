c3c3f797e09fc805a79a194960d232707bbbabff
keshavsharma25 <skeshav0825@gmail.com>
2026-03-02 18:38:50 +0530

    fix: treesitter install -> setup

--- lua/keshav/plugins/treesitter.lua
+++ lua/keshav/plugins/treesitter.lua
@@ -4,34 +4,39 @@ return {
     lazy = false,
     build = ':TSUpdate',
     config = function()
-        local config = require("nvim-treesitter")
-        config.install({
-            'bash',
-            'c',
-            'go',
-            'javascript',
-            'jsdoc',
-            'lua',
-            'python',
-            'rust',
-            'solidity',
-            'sway',
-            'typescript',
-            'vimdoc',
-            'html',
-            'latex',
-            'yaml',
-        }):wait(30000)
+        local config = require('nvim-treesitter.configs')
 
+        config.setup({
+            modules = {},
+            ignore_install = {},
+            ensure_installed = {
+                'bash',
+                'c',
+                'go',
+                'javascript',
+                'jsdoc',
+                'lua',
+                'python',
+                'rust',
+                'solidity',
+                'sway',
+                'typescript',
+                'vimdoc',
+                'html',
+                'latex',
+                'yaml',
+            },
+            sync_install = false, -- Set to true if you want it to block during install
+            auto_install = true, -- Automatically install missing parsers when entering buffer
 
-        --     sync_install = false,
-        --     indent = {
-        --         enable = true,
-        --     },
-        --     highlight = {
-        --         enable = true,
-        --         additional_vim_regex_highlighting = { 'markdown' },
-        --     },
-        -- })
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { 'markdown' },
+            },
+
+            indent = {
+                enable = true,
+            },
+        })
     end,
 }
