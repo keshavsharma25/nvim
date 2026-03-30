08983ff74033d022ad5830e47c70cfc89af3948f
keshavsharma25 <skeshav0825@gmail.com>
2026-03-31 00:10:48 +0530

    refactor: use nordic theme

--- lua/keshav/plugins/nordic.lua
+++ lua/keshav/plugins/nordic.lua
@@ -1,20 +1,34 @@
 return {
     'AlexvZyl/nordic.nvim',
-    lazy = true,
+    lazy = false,
     -- priority = 1000,
     config = function()
-        require('nordic').setup({
-            transparent_bg = true,
-            telescope = {
+        local nordic = require('nordic')
+        nordic.setup({})
+
+        local options = {
+            transparent = {
+                bg = true,
+                float = true,
+            },
+            on_palette = function(palette) end,
+            -- This callback can be used to override the colors used in the extended palette.
+            after_palette = function(palette) end,
+            -- This callback can be used to override highlights before they are applied.
+            on_highlight = function(highlights, palette) end,
+            bright_border = true,
+            bold_keywords = true,
+            italic_comments = true,
+            noice = {
                 -- Available styles: `classic`, `flat`.
-                styles = 'flat',
+                style = 'flat',
             },
             ts_context = {
                 -- Enables dark background for treesitter-context window
                 dark_background = true,
             },
-        })
+        }
 
-        require('nordic').load()
+        nordic.load(options)
     end,
 }
