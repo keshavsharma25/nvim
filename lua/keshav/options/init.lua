4ad143deefe9d4f5812dc7590442a948733bc917
keshavsharma25 <skeshav0825@gmail.com>
2026-06-10 00:43:05 +0530

    refactor: disable netrw

--- lua/keshav/options/init.lua
+++ lua/keshav/options/init.lua
@@ -31,9 +31,11 @@ vim.opt.scrolloff = 5
 
 vim.opt.hlsearch = true
 
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
+-- vim.g.netrw_browse_split = 0
+-- vim.g.netrw_banner = 0
+-- vim.g.netrw_winsize = 25
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 vim.g.swapfile = false
 
