8a0a4b194c5fbeb61227d76d8af31fae877a5859
keshavsharma25 <skeshav0825@gmail.com>
2026-07-31 00:23:19 +0530

    add diff treesitter

--- lua/keshav/plugins/treesitter.lua
+++ lua/keshav/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
                 ensure_installed = {
                     'bash',
                     'c',
+                    'diff',
                     'comment',
                     'go',
                     'javascript',
